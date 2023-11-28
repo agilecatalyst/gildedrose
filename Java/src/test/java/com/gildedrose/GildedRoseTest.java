38bddc92d3da44d6fe10be1957e5de03bc2384ca
Dirk Verstraete <dirk.verstraete.ext@delijn.be>
2023-11-28 18:22:29 +0100

    update first test and add comments to work on it when you feel like it

--- Java/src/test/java/com/gildedrose/GildedRoseTest.java
+++ Java/src/test/java/com/gildedrose/GildedRoseTest.java
@@ -7,11 +7,17 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 class GildedRoseTest {
 
     @Test
-    void foo() {
+    void nameAndValues() {
         Item[] items = new Item[] { new Item("foo", 0, 0) };
         GildedRose app = new GildedRose(items);
         app.updateQuality();
-        assertEquals("fixme", app.items[0].name);
+        assertEquals("foo", app.items[0].getName());
+        assertEquals(0, app.items[0].getSellIn());
+        assertEquals(0, app.items[0].getQuality());
     }
+
+    //TODO add a lot of tests (see TexttestFixture) - but I am not in the mood for it now...and I have a fulltime life next to the job
+    //TODO as a consultant on the beach it could maybe trigger me more ;-)
+    //As a senior I am in no mood today, headache and kids are my prio today...
 
 }
